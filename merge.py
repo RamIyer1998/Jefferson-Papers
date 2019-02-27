b48503e11e0feaee6fabbca54d3a93b236ccdb4c
ramiyer1998 <ramiyer1998@gmail.com>
2019-02-27 00:33:43 -0500

    Finished a preliminary version of merge.py

--- merge.py
+++ merge.py
@@ -1,12 +1,24 @@
 import sys
 import os
 
-def main():
-    files = os.listdir("Jefferson_Papers/from")
 
-    for file in files:
-        splits = file.split("thomas jefferson ")
-        os.rename("Jefferson_Papers/from/"+file, "Jefferson_Papers/from/"+splits[len(splits)-1])
+def main():
+    files = os.listdir("Jefferson_Papers/From")
+    merge_files = os.listdir("jeff/to")
+
+    for file in merge_files:
+        document = file.split(" ")
+        merge = True
+        for comp in files:
+            comp_doc = comp.split(" ")
+            for x in range(1, 5):
+                if document[len(document)-x] != comp_doc[len(comp_doc)-x]:
+                    break
+                if x == 4:
+                    merge = False
+        if merge:
+            os.rename("jeff/to/"+file, "Jefferson_Papers/From/"+file)
+
 
 if __name__ == '__main__':
     main()
