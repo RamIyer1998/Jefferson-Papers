8c974816d121bff3b85020e32a956a36d88fefad
Sriram Iyer <ramiyer1998@gmail.com>
2019-08-03 17:52:49 -0400

    Edit paths to accomodate new file structure

--- src/books.py
+++ src/books.py
@@ -5,15 +5,15 @@ import shutil
 def bookConversion():
     # Make a folder for all of the new txt files
     try:
-        os.mkdir("HRTC Converted Books")
+        os.mkdir("../HRTC Converted Books")
     except FileExistsError:
         print("aw shucks it already exists")
 
     # For each book's directory, compile all of the pages into one txt file
-    path = "./"+'HRTC Books'+"/TJBooks/"
+    path = "../"+'HRTC Books'+"/TJBooks/"
     for root, dirs, files in os.walk(path):
         for directory in sorted(dirs):
-            book_name = "./" + "HRTC Converted Books/" + directory +".txt"
+            book_name = "../" + "HRTC Converted Books/" + directory +".txt"
             with open(book_name, "ab") as outfile:
                 for x, y, z in os.walk(path + directory):
                     for name in sorted(z):
@@ -22,23 +22,23 @@ def bookConversion():
 
     # Make a folder for the new corpus
     try:
-        os.mkdir("corpora/books+letters")
+        os.mkdir("../corpora/books+letters")
         print("corpora/books+letters successfully created!")
     except FileExistsError:
         print("directory already exists")
 
     # Copy the Jefferson letters to the corpora folder
-    files = os.listdir("Jefferson Papers/From_Jefferson")
+    files = os.listdir("../Jefferson Papers/From_Jefferson")
     for f in files:
-        shutil.copyfile("Jefferson Papers/From_Jefferson/"+f, "corpora/books+letters/"+f)
+        shutil.copyfile("../Jefferson Papers/From_Jefferson/"+f, "corpora/books+letters/"+f)
 
     # Copy files from 'HRTC Converted Books' to the new corpora folder
-    files = os.listdir("HRTC Converted Books/")
+    files = os.listdir("../HRTC Converted Books/")
     for f in files:
-        shutil.copyfile("HRTC Converted Books/" + f, "corpora/books+letters/" + f)
+        shutil.copyfile("../HRTC Converted Books/" + f, "corpora/books+letters/" + f)
 
     # Delete 'HRTC Converted Books'
-    shutil.rmtree("HRTC Converted Books/")
+    shutil.rmtree("../HRTC Converted Books/")
 
 
 if __name__ == '__main__':
--- src/corpora.py
+++ src/corpora.py
@@ -7,43 +7,45 @@ def main():
 
     #Make directories
     try:
-        os.mkdir("corpora")
+        os.mkdir("../corpora")
         print("corpora created!")
     except FileExistsError:
         print("directory already exists")
 
     try:
-        os.mkdir("corpora/books+letters")
+        os.mkdir("../corpora/books+letters")
         print("corpora/books+letters successfully created!")
     except FileExistsError:
         print("directory already exists")
 
     try:
-        os.mkdir("corpora/letters+pages")
+        os.mkdir("../corpora/letters+pages")
         print("corpora/letters+pages successfully created!")
     except FileExistsError:
         print("directory already exists")
 
     #copy files
-    files = os.listdir("Jefferson Papers/To_Jefferson")
+    files = os.listdir("../Jefferson Papers/To_Jefferson")
     for f in files:
-        shutil.copyfile("Jefferson Papers/To_Jefferson/"+f, "corpora/books+letters/"+f)
-        shutil.copyfile("Jefferson Papers/To_Jefferson/"+f, "corpora/letters+pages/"+f)
+        shutil.copyfile("../Jefferson Papers/To_Jefferson/"+f, "../corpora/books+letters/"+f)
+        shutil.copyfile("../Jefferson Papers/To_Jefferson/"+f, "../corpora/letters+pages/"+f)
 
-    files = os.listdir("Jefferson Papers/From_Jefferson")
+    files = os.listdir("../Jefferson Papers/From_Jefferson")
     for f in files:
-        shutil.copyfile("Jefferson Papers/From_Jefferson/"+f, "corpora/books+letters/"+f)
-        shutil.copyfile("Jefferson Papers/From_Jefferson/"+f, "corpora/letters+pages/"+f)
+        shutil.copyfile("../Jefferson Papers/From_Jefferson/"+f, "../corpora/books+letters/"+f)
+        shutil.copyfile("../Jefferson Papers/From_Jefferson/"+f, "../corpora/letters+pages/"+f)
 
 
-    for root, dirs, files in os.walk("HRTC Books/TJBooks"):
+    for root, dirs, files in os.walk("../HRTC Books/TJBooks"):
         for directory in dirs:
-            shutil.copytree("HRTC Books/TJBooks/" + directory, "corpora/books+letters/"+directory)
-            files = os.listdir("HRTC Books/TJBooks/"+directory)
+            shutil.copytree("../HRTC Books/TJBooks/" + directory, "../corpora/books+letters/"+directory)
+            files = os.listdir("../HRTC Books/TJBooks/"+directory)
             for f in files:
                 newName = directory+f
-                shutil.copyfile("HRTC Books/TJBooks/"+directory+"/"+f, "corpora/letters+pages/"+newName)
+                shutil.copyfile("../HRTC Books/TJBooks/"+directory+"/"+f, "../corpora/letters+pages/"+newName)
 
+    shutil.rmtree("../Jefferson Papers/")
+    shutil.rmtree("../jeff")
 
 if __name__ == '__main__':
     main()
